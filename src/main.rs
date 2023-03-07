ba66e42c4f1931f5a6714565f012b5c1f173ca75
Muhammad Mominul Huque <mominul2082@gmail.com>
2023-03-07 09:25:30 +0600

    Divide it into a lib

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,24 @@
-use axum::{extract::Path, headers::UserAgent, routing::get, Router, TypedHeader};
-use std::fmt::Write;
+use packhub::app;
+use tracing::{Level, info};
+use tracing_subscriber::FmtSubscriber;
 use std::net::SocketAddr;
-use tower_http::trace::{DefaultMakeSpan, TraceLayer};
-
-mod detect;
-mod platform;
-
-async fn handler(
-    Path((owner, repo)): Path<(String, String)>,
-    TypedHeader(agent): TypedHeader<UserAgent>,
-) -> String {
-    let mut response = String::new();
-
-    let octocrab = octocrab::instance();
-    let rel = octocrab
-        .repos(&owner, &repo)
-        .releases()
-        .get_latest()
-        .await
-        .unwrap();
-
-    let name = rel.name.clone().unwrap();
-    write!(
-        &mut response,
-        "Project name: {repo}\nLatest Release: {name}\nAssets:\n"
-    )
-    .unwrap();
-
-    for asset in rel.assets {
-        write!(
-            &mut response,
-            "{} - {}\n",
-            asset.name,
-            asset.browser_download_url.as_str()
-        )
-        .unwrap();
-    }
-    write!(&mut response, "\n\nUser Agent: {agent}").unwrap();
-
-    response
-}
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::DEBUG)
+        .finish();
 
-    let app = Router::new()
-        .route("/apt/github/:owner/:repo", get(handler))
-        .layer(
-            TraceLayer::new_for_http().make_span_with(DefaultMakeSpan::new().include_headers(true)),
-        );
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    info!("listening on {}", addr);
 
     // run it
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("listening on {}", addr);
     axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+        .serve(app().into_make_service())
         .await
         .unwrap();
 }
