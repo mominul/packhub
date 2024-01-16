5cd61add8696a8ba73b552ef763e52dd4f2ae421
Muhammad Mominul Huque <mominul2082@gmail.com>
2024-01-17 01:17:26 +0600

    Implement repo metadata index of RPM

--- src/rpm/index.rs
+++ src/rpm/index.rs
@@ -1,38 +1,95 @@
 use askama::Template;
+use chrono::Utc;
+use sha2::Sha256;
+use zstd::encode_all;
 
-use super::package::{Dependency, RPMPackage};
+use crate::utils::hashsum;
 
-#[derive(Debug, Template)]
+use super::package::RPMPackage;
+
+#[derive(Template)]
 #[template(path = "primary.xml")]
-struct Primary {
-    packages: Vec<RPMPackage>,
+struct Primary<'a> {
+    packages: &'a [RPMPackage],
 }
 
-#[derive(Debug, Template)]
+#[derive(Template)]
 #[template(path = "filelists.xml")]
-struct FileLists {
-    packages: Vec<RPMPackage>,
+struct FileLists<'a> {
+    packages: &'a [RPMPackage],
 }
 
-#[derive(Debug, Template)]
+#[derive(Template)]
 #[template(path = "other.xml")]
-struct Other {
-    packages: Vec<RPMPackage>,
+struct Other<'a> {
+    packages: &'a [RPMPackage],
 }
 
-pub fn get_primary_index(packages: Vec<RPMPackage>) -> String {
+#[derive(Template)]
+#[template(path = "repomd.xml")]
+struct RepoMD {
+    primary: Metadata,
+    filelists: Metadata,
+    other: Metadata,
+    timestamp: i64,
+}
+
+struct Metadata {
+    sha256: String,
+    open_sha256: String,
+    size: usize,
+    open_size: usize,
+}
+
+pub fn get_primary_index(packages: &[RPMPackage]) -> String {
     let primary = Primary { packages };
     primary.render().unwrap()
 }
 
-pub fn get_filelists_index(packages: Vec<RPMPackage>) -> String {
+pub fn get_filelists_index(packages: &[RPMPackage]) -> String {
     let list = FileLists { packages };
     list.render().unwrap()
 }
 
-pub fn get_other_index(packages: Vec<RPMPackage>) -> String {
+pub fn get_other_index(packages: &[RPMPackage]) -> String {
     let list = Other { packages };
     list.render().unwrap()
+}
+
+pub fn get_repomd_index(packages: &[RPMPackage]) -> String {
+    let primary = get_primary_index(packages);
+    let filelists = get_filelists_index(packages);
+    let other = get_other_index(packages);
+
+    let repomd = RepoMD::create(primary, filelists, other);
+
+    repomd.render().unwrap()
+}
+
+impl Metadata {
+    /// Create the metadata of the `content`.
+    fn create(content: String) -> Metadata {
+        let data = content.as_bytes();
+        let open_size = data.len();
+        let open_sha256 = hashsum::<Sha256>(data);
+        let compressed = encode_all(data, 0).unwrap();
+        let size = compressed.len();
+        let sha256 = hashsum::<Sha256>(&compressed);
+
+        Metadata { sha256, open_sha256, size, open_size }
+    }
+}
+
+impl RepoMD {
+    fn create(primary: String, filelists: String, other: String) -> RepoMD {
+        let primary = Metadata::create(primary);
+        let filelists = Metadata::create(filelists);
+        let other = Metadata::create(other);
+        let timestamp = Utc::now().timestamp();
+
+
+        RepoMD { primary, filelists, other, timestamp }
+    }
 }
 
 #[cfg(test)]
@@ -54,10 +111,19 @@ mod tests {
         let package = RPMPackage::from_package(&package).unwrap();
         let packages = vec![package];
 
-        assert_snapshot!(get_primary_index(packages.clone()));
+        assert_snapshot!(get_primary_index(&packages));
 
-        assert_snapshot!(get_filelists_index(packages.clone()));
+        assert_snapshot!(get_filelists_index(&packages));
 
-        assert_snapshot!(get_other_index(packages));
+        assert_snapshot!(get_other_index(&packages));
+
+        let repomd = get_repomd_index(&packages);
+        insta::with_settings!({filters => vec![
+            // Date is a changing value, so replace it with a hardcoded value.
+            (r"<timestamp>.+<\/timestamp>", r"<timestamp>TIMESTAMP<\/timestamp>"),
+            (r"<revision>.+<\/revision>", r"<revision>TIMESTAMP<\/revision>"),
+        ]}, {
+            assert_snapshot!(repomd);
+        });
     }
 }
