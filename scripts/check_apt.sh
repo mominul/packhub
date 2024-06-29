25431d91e8376ffec3a1a679c9f35b08d81a3d03
Muhammad Mominul Huque <mominul2082@gmail.com>
2024-06-30 01:30:02 +0600

    capture return value of the script

--- scripts/check_apt.sh
+++ scripts/check_apt.sh
@@ -6,6 +6,14 @@ apt install sudo wget -y
 echo
 echo "Running the package key and repository setup script"
 wget -qO- http://host.docker.internal:3000/sh/github/ubuntu/mominul/pack-exp3 | sh
+return_value=$?
+
+if [ $return_value -ne 0 ]; then
+    echo "The script failed with exit code $return_value"
+    # Handle error case here
+else
+    echo "The script completed successfully"
+fi
 echo "Package key and repository setup script ran successfully."
 
 output=$(apt search openbangla 2>&1)
