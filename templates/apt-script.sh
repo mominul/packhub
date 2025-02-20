df72cd99363772c9fa51e25a4407e6e1b829e4dc
Muhammad Mominul Huque <mominul2082@gmail.com>
2025-02-20 14:37:02 +0600

    update

--- templates/apt-script.sh
+++ templates/apt-script.sh
@@ -8,7 +8,7 @@ echo "Downloading and installing the repository key..."
 wget -qO- {{host}}/v1/keys/packhub.gpg | sudo tee /etc/apt/keyrings/packhub.gpg > /dev/null
 echo
 echo "Adding the repository to your system..."
-echo "deb [signed-by=/etc/apt/keyrings/packhub.gpg] {{host}}/v1/apt/{{distro}}/github/{{owner}}/{{repo}} stable main" > /etc/apt/sources.list.d/{{repo}}.list
+sudo echo "deb [signed-by=/etc/apt/keyrings/packhub.gpg] {{host}}/v1/apt/{{distro}}/github/{{owner}}/{{repo}} stable main" > /etc/apt/sources.list.d/{{repo}}.list
 echo 
 echo "Updating package lists..."
 sudo apt-get update
