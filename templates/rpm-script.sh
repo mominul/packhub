56f586670808868af7a57f61be63d5c7a028a418
Muhammad Mominul Huque <mominul2082@gmail.com>
2025-03-27 13:48:49 +0600

    Implemented scripted repository addition for rpm

--- templates/rpm-script.sh
+++ templates/rpm-script.sh
@@ -1,1 +1,12 @@
-sh -c 'echo -e "[{{cased}}]\nname={{repo}}\nbaseurl=http://packhub.dev/v1/rpm/github/{{owner}}/{{repo}}\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=http://packhub.dev/v1/keys/packhub.asc" > /etc/yum.repos.d/{{cased}}.repo'
+#!/bin/sh
+
+echo "Welcome to package key and repository setup script for {{repo}}"
+echo "This script will add the repository key and repository to your system."
+echo "Please make sure you have sudo access to run this script."
+
+echo -e "[{{repo}}]\nname={{repo}}\nbaseurl={{host}}/v1/rpm/github/{{owner}}/{{repo}}\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey={{host}}/v1/keys/packhub.asc" | sudo tee /etc/yum.repos.d/{{repo}}.repo > /dev/null
+
+echo
+echo "Repository has been added to your system."
+echo "Please update your package lists to start using the repository."
+echo "Use 'dnf update' or 'yum update' or 'zypper refresh' depending on your package manager."
